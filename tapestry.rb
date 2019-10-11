ef99c5041040b3ead8f95bbdb155b80bb6ff1fe6
Marek Fořt <marek.fort@ackee.cz>
2019-10-11 16:55:48 +0200

    Update install script and sha.

--- tapestry.rb
+++ tapestry.rb
@@ -2,14 +2,13 @@ class Tapestry < Formula
     desc "CLI tool to create lightweight Swift frameworks 🏎"
     homepage "https://github.com/AckeeCZ/tapestry"
     url "https://github.com/AckeeCZ/tapestry/archive/0.0.1.tar.gz"
-    sha256 "28a76bb9c97e55e93ddba01022bf9a0e46f3d875d33561a76b08ef98346381ce"
+    sha256 "1682ec4de54eaa0b7a9412b6bad95894d0679310f579e4215acdb55633bc9e0f"
     head "https://github.com/AckeeCZ/tapestry.git"
 
     depends_on :xcode => ["8.0", :build]
   
     def install
-        system "swift", "build", "--disable-sandbox", "-c", "release"
-        bin.install ".build/release/tapestry"
+        bin.install "tapestry"
     end
 
     test do
