0d4f7b60abc7fa0db0dfb0bfde0ba742c41dc681
Marek Fořt <marek.fort@ackee.cz>
2019-10-11 17:01:58 +0200

    Download tapestry binary.

--- tapestry.rb
+++ tapestry.rb
@@ -1,7 +1,7 @@
 class Tapestry < Formula
     desc "CLI tool to create lightweight Swift frameworks 🏎"
     homepage "https://github.com/AckeeCZ/tapestry"
-    url "https://github.com/AckeeCZ/tapestry/archive/0.0.1.tar.gz"
+    url "https://github.com/AckeeCZ/tapestry/releases/download/0.0.1/tapestry-0.0.1.zip"
     sha256 "1682ec4de54eaa0b7a9412b6bad95894d0679310f579e4215acdb55633bc9e0f"
     head "https://github.com/AckeeCZ/tapestry.git"
 
