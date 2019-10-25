237191705e4e95e5a336ab6e8c04a83cefc3e69f
Marek Fořt <marek.fort@ackee.cz>
2019-10-25 13:41:51 +0200

    Update sha256.

--- tapestry.rb
+++ tapestry.rb
@@ -2,7 +2,7 @@ class Tapestry < Formula
     desc "CLI tool to create lightweight Swift frameworks 🏎"
     homepage "https://github.com/AckeeCZ/tapestry"
     url "https://github.com/AckeeCZ/tapestry/releases/download/0.0.2/tapestry-0.0.2.zip"
-    sha256 "fdd6e6862c0a5498dc2080208d8c228dd752632217ed70513c97592992e85b75"
+    sha256 "1452414607e0ad09b9e58b84b6300519af8430ee23f5e919e584acbde963eddc"
     head "https://github.com/AckeeCZ/tapestry.git"
 
     depends_on :xcode => ["8.0", :build]
