b206134c268df48a8c050bc92781022188ade1aa
Marek Fořt <marek.fort@ackee.cz>
2019-11-11 14:06:37 +0100

    Update version.

--- tapestry.rb
+++ tapestry.rb
@@ -1,7 +1,7 @@
 class Tapestry < Formula
     desc "CLI tool to create lightweight Swift frameworks 🏎"
     homepage "https://github.com/AckeeCZ/tapestry"
-    url "https://github.com/AckeeCZ/tapestry/releases/download/0.0.2/tapestry-0.0.2.zip"
+    url "https://github.com/AckeeCZ/tapestry/releases/download/0.0.4/tapestry-0.0.4.zip"
     sha256 "0c345215b7ec75020652b3e2cada22be86d915c26592ef5636a25300ae2c0cc7"
     head "https://github.com/AckeeCZ/tapestry.git"
 
